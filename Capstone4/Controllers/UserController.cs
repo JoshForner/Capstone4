ee0dd155821ee4cfecedba7ebda27b654f5f134e
Josh Forner <fornerjos@gmail.com>
2020-05-31 22:58:55 -0400

    First Commit

---
+++ Capstone4/Controllers/UserController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Capstone4.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+
+namespace Capstone4.Controllers
+{
+    [Authorize]
+    public class UserController : Controller
+    {
+        private readonly UsersContext _context;
+        public UserController(UsersContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var user = User.Identity;
+            return View(user);
+        }
+
+        public IActionResult TaskList()
+        {
+            string id = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var stuff = _context.Tasks.Where(x => x.UserId == id).ToList();
+            return View(stuff);
+        }
+        
+       
+        public IActionResult MarkComplete(int id)
+        {
+            Tasks found = _context.Tasks.Find(id);
+            if(found != null)
+            {
+                found.TaskComplete = !found.TaskComplete;
+                _context.Entry(found).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                _context.Update(found);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("TaskList", new { id = found.Id });
+        }
+
+        public IActionResult Delete(int id)
+        {
+            Tasks found = _context.Tasks.Find(id);
+            if (found != null)
+            {
+                _context.Tasks.Remove(found);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("TaskList", new { id = found.Id });
+        }
+
+        [HttpGet]
+        public IActionResult AddTask()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddTask(Tasks newTask)
+        {
+            if (ModelState.IsValid)
+            {
+                newTask.UserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+                _context.Tasks.Add(newTask);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("TaskList", new { id = newTask.Id });
+        }
+
+        public IActionResult Sort(string order)
+        {
+            var userid = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var newsort = _context.Tasks.Where(x => x.UserId == userid);
+            if(order == "asc")
+            {
+               newsort = newsort.OrderBy(x => x.TaskDue);
+            }
+            else
+            {
+                newsort = newsort.OrderByDescending(x => x.TaskDue);
+            }
+            HttpContext.Session.SetString("Order", order);
+            return View("TaskList", newsort.ToList());
+        }
+
+        public IActionResult Sort2(string complete)
+        {
+            var userid = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var newsort = _context.Tasks.Where(x => x.UserId == userid);
+            if (complete == "true")
+            {
+                newsort = newsort.OrderBy(x => x.TaskComplete);
+            }
+            else
+            {
+                newsort = newsort.OrderByDescending(x => x.TaskComplete);
+            }
+            HttpContext.Session.SetString("Complete", complete);
+            return View("TaskList", newsort.ToList());
+        }
+
+        public IActionResult FindTask(string TDescription)
+        {
+            var userid = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var newsort = from x in _context.Tasks where x.TaskDescription.Contains(TDescription) &&  x.UserId == userid select x;
+            return View("TaskList", newsort.ToList());
+        }
+    }
+}
---
+++ Capstone4/Models/Tasks.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Capstone4.Models
+{
+    public partial class Tasks
+    {
+        public int Id { get; set; }
+        public string TaskName { get; set; }
+        public string TaskDescription { get; set; }
+        public DateTime TaskDue { get; set; }
+        public bool TaskComplete { get; set; }
+        public string UserId { get; set; }
+
+        public virtual AspNetUsers User { get; set; }
+    }
+}
